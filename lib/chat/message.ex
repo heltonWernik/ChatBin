dd912cb1bc6860a04e377f834543eee6dc96f46a
Helton Wernik <helton.wernik@docdok.health>
2018-05-09 11:07:37 +0300

    firsts commit

---
+++ lib/chat/message.ex
@@ -0,0 +1,27 @@
+defmodule Chat.Message do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Chat.Message
+
+
+  schema "messages" do
+    field :message, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(message, attrs) do
+    message
+    |> cast(attrs, [:name, :message])
+    |> validate_required([:name, :message])
+  end
+
+  def get_messages(limit \\ 100) do
+    Chat.Repo.all(Message, limit: limit)
+  end
+end
+
+
+
